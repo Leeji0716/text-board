e3e2d1f05994d70912d36e6708395e18a3d94f3d
Dumbbell04 <Dumbbelloper04@gmail.com>
2024-03-20 16:09:00 +0900

    json delete finish

--- src/main/java/org/example/base/BoardApp.java
+++ src/main/java/org/example/base/BoardApp.java
@@ -1,11 +1,13 @@
 package org.example.base;
 
-import org.example.domain.ArticleController;
+import org.example.domain.article.controller.ArticleController;
+import org.example.domain.article.controller.ArticleTestController;
 
 import java.util.Scanner;
 
 public class BoardApp {
-    ArticleController articleController = new ArticleController();
+//    ArticleController articleController = new ArticleController();
+    ArticleTestController articleController = new ArticleTestController();
     Scanner scan = new Scanner(System.in);
     public void run() {
 
@@ -21,10 +23,10 @@ public class BoardApp {
             switch (cmd) {
                 case "add" -> articleController.add();
                 case "list" -> articleController.list();
-                case "update" -> articleController.update();
+//                case "update" -> articleController.update();
                 case "delete" -> articleController.delete();
-                case "detail" -> articleController.detail();
-                case "search" -> articleController.search();
+//                case "detail" -> articleController.detail();
+//                case "search" -> articleController.search();
                 default -> System.out.println("올바른 명령어가 아닙니다.");
             }
         }
---
+++ src/main/java/org/example/domain/article/model/ArticleFileRepository.java
@@ -0,0 +1,107 @@
+package org.example.domain.article.model;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.base.CommonUtil;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArticleFileRepository {
+    private int latestId = 1;
+    private ArrayList<Article> articleList = new ArrayList<>(); //저장소
+    private final String jsonFilePath = "article.json";
+
+    public Article findById(int id) {
+        //id에 해당하는 게시물 (article) 반환
+        for (int i = 0; i < articleList.size(); i++) {
+            Article article = articleList.get(i);
+
+            if (article.getId() == id) {
+                return article;
+            }
+        }
+        return null; // null -> 없다. 객체 타입에서만 사용 가능
+
+    }
+    public void deleteArticle(Article article) {
+        ObjectMapper mapper = new ObjectMapper();
+        File file = new File(jsonFilePath);
+
+        try {
+            // JSON 파일을 읽어와 ArrayList로 변환
+            ArrayList<Article> dataList = mapper.readValue(file, new TypeReference<ArrayList<Article>>() {});
+            // 삭제하려는 요소를 식별하여 배열에서 제거
+            Iterator<Article> iterator = dataList.iterator();
+            while (iterator.hasNext()) {
+                Article data = iterator.next();
+                if (data.getId() == article.getId()) { // 삭제하려는 요소의 조건을 지정
+                    iterator.remove(); // 배열에서 해당 요소를 제거
+                }
+            }
+            // 변경된 배열을 다시 JSON 파일에 씀
+            mapper.writeValue(file, dataList);
+            System.out.println("JSON 파일에서 요소가 성공적으로 삭제되었습니다.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public ArticleFileRepository(){
+        this.articleList = loadPostsFromFile("article.json");
+        if(articleList.size() == 0){
+            latestId = 0;
+            return;
+        }
+        int index = (this.articleList.size() - 1); //개수 -1. 마지막 인덱스
+        Article article = articleList.get(index);
+        latestId = article.getId();
+    }
+    private ArrayList<Article> loadPostsFromFile(String filePath){
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayList<Article> data = new ArrayList<>();
+        try {
+            // JSON 파일을 읽어와 ArrayList<Article>으로 변환
+            data = mapper.readValue(new File("article.json"), new TypeReference<ArrayList<Article>>() {});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+    public void saveArticle(String title, String body){
+        // 번호는 latestID, 제목이 title, 내용이 body, 조회수 0, 등록날짜 현재시간
+        //json 파일로 저장
+
+        latestId++; //번호 증가
+
+        CommonUtil commonUtil = new CommonUtil();
+        Article a1 = new Article(latestId, title, body, 0, commonUtil.getCurrentDateTime());
+        articleList.add(a1); //Article 먼저 리스트에 담기
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            // 객체를 JSON 형태로 변환하여 파일에 저장
+            mapper.writeValue(new File("article.json"), articleList);
+            System.out.println("객체가 JSON 형태로 파일에 저장되었습니다.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public ArrayList<Article> findAll() {
+        //json 파일을 읽어와서 ArrayList로 반환
+        ArrayList<Article> data = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            // JSON 파일을 읽어와 ArrayList<Article>으로 변환
+            data = mapper.readValue(new File("article.json"), new TypeReference<ArrayList<Article>>() {});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+}
